f488efe3db75e96474c7c15299d380aaa3ac816c
JackRFuller <jackrfuller@outlook.com>
2015-07-28 13:46:16 +0100

    Basic Mechanic Prototyped
    The basic mechanic has been prototyped and implemented into the scene

---
+++ Beacon/Assets/Scripts/Enemies/EnemyBaseClass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBaseClass : MonoBehaviour {
+
+	[Header("Enemy Navigation Attributes")]
+	public Transform Target;
+	public NavMeshAgent NVM_Agent;
+	
+	[Header("Enemy Movement Attributes")]
+	public Rigidbody RB;
+	public float MovementSpeed;
+	public float  Health;
+
+}
---
+++ Beacon/Assets/Scripts/Player/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	[Header("Shooting Variables")]
+	[SerializeField] private float Range;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButton(0))
+		{
+			Fire();
+		}
+	
+	}
+
+	void Fire()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F,0.5F,0));
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, Range))
+		{
+			hit.collider.gameObject.SendMessage("Hit", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
